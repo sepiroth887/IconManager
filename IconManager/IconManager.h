19b0b5ad9b67aa013ad28389d99ce42b40eeb4d1
Tobias Haag <tobias@onedrum.com>
2012-05-09 13:45:47 +0100

    Initial version of icon setting in osx 	- set icon on a file / folder 	- set badge on a file / folder 	- remove icon from a file

--- IconManager/IconManager.h
+++ IconManager/IconManager.h
@@ -3,11 +3,20 @@
 //  IconManager
 //
 //  Created by Tobias Haag on 5/8/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 Yammer. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
 @interface IconManager : NSObject
 
++ (NSImage*)getCurrentIcon:(NSString*) file;
++ (int)setBadgeForFile:(NSString*) file AndIconPath:(NSString*)iconPath;
++ (int)setIconForFile:(NSString*) file AndIconPath:(NSString*)iconPath;
++ (int)removeIconFromFile:(NSString*)file;
+
 @end
+
+int setBadge(const char* filepath, const char* iconPath);
+int setIcon(const char* filepath, const char* iconPath);
+int removeIcon(const char* filepath);
